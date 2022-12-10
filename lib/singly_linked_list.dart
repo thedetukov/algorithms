523fef354d71b50e71c161313cc081920c7a2184
Dmitry Detukov <dmitry.detukov@zxteam.net>
2022-12-10 17:09:32 +0200

    Implement function add

--- lib/singly_linked_list.dart
+++ lib/singly_linked_list.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: unnecessary_this
 class SinglyLinkedList<T> {
-  int get length => _length;
+  int get length => this._length;
   int _length = 0;
 
   void add(T) {
     this._length = this._length + 1;
+    SinglyLinkedList myList = SinglyLinkedList();
   }
 }
+
