8a3673c4dd3e7a663baf89cc5a9faeaf2e67c460
Dmitry Detukov <dmitry.detukov@zxteam.net>
2022-12-10 13:50:51 +0200

    Implementation of the first two test cases

--- lib/singly_linked_list.dart
+++ lib/singly_linked_list.dart
@@ -1,6 +1,11 @@
-class SinglyLinkedList<T> {
-  int get length => 0;
+// ignore_for_file: unnecessary_this
 
-  void add(T) {}
+class SinglyLinkedList<T> {
+  // ignore: recursive_getters
+  int get length => _length;
+  int _length = 0;
+
+  void add(T) {
+    this._length = this._length + 1;
   }
-  
+}
