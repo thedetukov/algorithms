46827d3788d39e08d5fcfd45ebff69dec7246e52
Dmitry Detukov <dmitry.detukov@zxteam.net>
2022-12-14 10:21:22 +0200

    Added class SinglyLinkedListNode

--- lib/singly_linked_list.dart
+++ lib/singly_linked_list.dart
@@ -6,15 +6,30 @@
 class SinglyLinkedList<T> {
   int get length => this._length;
   int _length = 0;
+  final SinglyLinkedList<T> myList = SinglyLinkedList<T>();
+  get value => myList;
 
+  // set addedNode(T addedNode) {}
+
+  ///
+  /// This method adds the given value to the end of the list
+  ///
   void addLast(T value) {
     this._length = this._length + 1;
-    print("Adding value: '$value' into ends of the list");
-    // SinglyLinkedList myList = SinglyLinkedList();
+    print("Adding value: '$value' into ends of the list"); 
   }
 
-    void clear() {
-    this._length =0;
-    // SinglyLinkedList myList = SinglyLinkedList();
+  ///
+  /// This method removes a value from the end of the list
+  ///
+  void clear() {
+    this._length = 0;
   }
+}
+
+///
+/// The class implements node management for a Singly Linked List.
+///
+class SinglyLinkedListNode<T> extends SinglyLinkedList {
+  final SinglyLinkedListNode<T> addedNode = SinglyLinkedListNode<T>();
 }
